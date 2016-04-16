2a4d266cebcd7b0362ce49803419ddb9cb5f92fd
andyafter <andyafter@gmail.com>
2016-04-16 19:22:20 +0800

    Fixed multiple.sh.

--- src/multiple.sh
+++ src/multiple.sh
@@ -1,36 +1,36 @@
 ./dirpole 1000
-cp image.pfm > image1.pfm
+cp image.pfm  image1.pfm
 ./dirpole 2000
-cp image.pfm > image2.pfm
+cp image.pfm  image2.pfm
 ./dirpole 3000
-cp image.pfm > image3.pfm
+cp image.pfm  image3.pfm
 ./dirpole 4000
-cp image.pfm > image4.pfm
+cp image.pfm  image4.pfm
 ./dirpole 5000
-cp image.pfm > image5.pfm
+cp image.pfm  image5.pfm
 ./dirpole 6000
-cp image.pfm > image6.pfm
+cp image.pfm  image6.pfm
 ./dirpole 7000
-cp image.pfm > image7.pfm
+cp image.pfm  image7.pfm
 ./dirpole 8000
-cp image.pfm > image8.pfm
+cp image.pfm  image8.pfm
 ./dirpole 9000
-cp image.pfm > image9.pfm
+cp image.pfm  image9.pfm
 ./dirpole 10000
-cp image.pfm > image10.pfm
+cp image.pfm  image10.pfm
 ./dirpole 11000
-cp image.pfm > image11.pfm
+cp image.pfm  image11.pfm
 ./dirpole 12000
-cp image.pfm > image12.pfm
+cp image.pfm  image12.pfm
 ./dirpole 13000
-cp image.pfm > image13.pfm
+cp image.pfm  image13.pfm
 ./dirpole 20000
-cp image.pfm > image14.pfm
+cp image.pfm  image14.pfm
 ./dirpole 30000
-cp image.pfm > image15.pfm
+cp image.pfm  image15.pfm
 ./dirpole 40000
-cp image.pfm > image16.pfm
+cp image.pfm  image16.pfm
 ./dirpole 50000
-cp image.pfm > image17.pfm
+cp image.pfm  image17.pfm
 ./dirpole 100000
-cp image.pfm > image18.pfm
+cp image.pfm  image18.pfm
